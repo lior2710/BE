33c390da4eaab7badde23edf29523bdae979147a
Nir Weber <webernir@bina-fin.co.il>
2017-05-25 13:37:18 +0300

    added button for RTL tests

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,25 @@ import RTFInput from './Components/RTFInput/RTFInput.jsx';
 
 // @observer
 class App extends Component {
+
+  toggleDirection = e => {
+    document.dir = document.dir === 'rtl' ? 'ltr' : 'rtl'
+  }
+
   render() {
     return (
       <div>
         <RTFInput
-            value="Test Value"
-            style={{maxWidth: '400px', margin: '20px'}}
-            className="be-rtf-editor"
-            onBlur ={(e, value, rtfInput) => console.log(e, value, rtfInput)}
-            onFocus={(e, value, rtfInput) => console.log(e, value, rtfInput)}
+          value="Test Value"
+          style={{ maxWidth: '400px', margin: '20px' }}
+          className="be-rtf-editor"
+          onBlur={(e, value, rtfInput) => console.log(e, value, rtfInput)}
+          onFocus={(e, value, rtfInput) => console.log(e, value, rtfInput)}
         />
         <DevTools />
+        <button onClick={this.toggleDirection} >
+          Toggle Direction
+        </button>
       </div>
     );
   }
